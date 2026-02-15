c10e801e41bdee2b7d784c4ce064299ccb3048cc
Abhishek Ranjan <ahisheksdhr62@gmail.com>
2026-02-15 19:18:20 +0530

    adding all images on about pagee

--- src/components/about.js
+++ src/components/about.js
@@ -253,7 +253,7 @@ const quoteRef = useRef(null);
 
             <img
                 ref={expImgRef}
-                src="/experience.jpg"
+                src="/assets/experience.jpg"
                 alt="Cybersecurity expertise"
                 className="experience-img"
                 />
@@ -294,14 +294,14 @@ const quoteRef = useRef(null);
           {/* LEFT VISUAL */}
           <div className="expertise-visual">
             <img
-              src="/expert.jpg"
+              src="/assets/expert.jpg"
               alt="Security Operations Center"
               className="img-main"
             />
 
             <img
             
-              src="/download1.jpg"
+              src="/assets/download1.jpg"
               alt="Cybersecurity shield"
               className="img-overlay"
             />
@@ -372,7 +372,7 @@ const quoteRef = useRef(null);
           </div>
 
           <div class="overview-image">
-            <img src="./team1.jpeg" alt="Security monitoring center" />
+            <img src="/assets/team1.jpeg" alt="Security monitoring center" />
           </div>
         </div>
       </section>
